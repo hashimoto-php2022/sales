72c7289cedd03923c5798b8f499d181492e93c5e
hashimoto-php2022 <2022php42-student003@la-study.com>
2022-05-18 17:59:25 +0900

    [add]Fortify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/app.css" rel="stylesheet">
-    <title>Document</title>
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
     <button class="bg-red-400">テスト</button>
