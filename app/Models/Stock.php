49471d9bbcaa6668a54911499b1b28139a12154c
ayu4413 <2022php42-student022@la-study.com>
2022-05-19 15:43:16 +0900

    Konta

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Stock extends Model
 {
     use HasFactory;
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
