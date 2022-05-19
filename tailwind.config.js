235345de49514174a9028a3987e736f03333d860
ike-ops111 <2022php42-student010@la-study.com>
2022-05-19 10:06:24 +0900

    IKEGAWA

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
