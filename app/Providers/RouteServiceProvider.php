ce04149f3534db661cfa5ca3cee1a92a4ce1d29c
hashimoto-php2022 <105625042+hashimoto-php2022@users.noreply.github.com>
2022-05-20 14:47:13 +0900

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/stock';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
